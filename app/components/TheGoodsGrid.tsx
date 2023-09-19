96f67bf568715608aeefb7bec2d66ed4660dd0eb
speedo-ua <mimikadpua@gmail.com>
2023-09-19 10:24:22 +0300

    some fix

--- app/components/TheGoodsGrid.tsx
+++ app/components/TheGoodsGrid.tsx
@@ -1,10 +1,27 @@
+'use client'
 import Reload from "./Reload";
 import Pagination from "./Pagination";
 import TheGoodsTile from "./TheGoodsTile";
+import { useEffect, useState } from "react";
+import Data from '@/api/data/goods.json'
 
 
 const TheGoodsGrid = () => {
 
+  const [result, setResult] = useState<any>()
+
+  useEffect(()=>{
+    dataFetching(Data)
+  }, [])
+
+
+  const dataFetching = async (url: any) => {
+    const res = await fetch(url)
+    const result = res.json()
+    setResult(result)
+  }
+
+  console.log (result)
     return(
         <div className="catalog-grid">
         <div className="catalog-grid_container">
